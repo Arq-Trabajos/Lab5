57372e37e6b27da6be49447b63f9d34fdc82713d
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-11-29 18:29:41 -0500

    avance dmem

---
+++ arm.v
@@ -0,0 +1,43 @@
+module top (
+    ports
+);
+    
+endmodule
+
+module dmem (
+    input clk, input [31:0] MemWrite, input [31:0] ALUResult, [31:0] WriteData,
+    output [31:0] ReadData
+);
+    
+endmodule
+
+module imem (
+    ports
+);
+    
+endmodule
+
+module controller (
+    ports
+);
+    
+endmodule
+
+module datapath (
+    ports
+);
+    
+endmodule
+
+module decode (
+    ports
+);
+    
+endmodule
+
+module condlogic (
+    output RegWrite, MemWrite, PCSrc
+);
+    
+endmodule
+
